74fcd9ef25665fc01cd8954d002a3fdd295b8fac
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2021-12-01 15:41:37 +0530

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,60 +1,52 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2020 Yusuf Usta
+
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+
+WhatsAsena - Yusuf Usta
 */
 
 const simpleGit = require('simple-git');
 const git = simpleGit();
-const amazone = require('../events');
+const Jimbrootan = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 const Config = require('../config');
 const exec = require('child_process').exec;
 const Heroku = require('heroku-client');
 const { PassThrough } = require('stream');
-const heroku = new Heroku({ token: Config.HEROKU.API_KEY })
 const axios = require('axios');
+const heroku = new Heroku({ token: Config.HEROKU.API_KEY })
 const Language = require('../language');
 const Lang = Language.getString('updater');
 
-//උස්සන උන්ගෙ රෙපෝ කෙලවිලාම පලයම්
-var logoimage = new Array ();
+var image = new Array ();
+image[0] = "https://telegra.ph/file/a31c6999517d3d74a1a87.jpg";
+image[1] = "https://telegra.ph/file/a31c6999517d3d74a1a87.jpg";
 
-logoimage[0] = "https://telegra.ph/file/a51a91c22de055d730c6e.jpg";
-logoimage[1] = "https://telegra.ph/file/34ef0c8009b176cd5772d.jpg";
-logoimage[2] = "https://telegra.ph/file/75433f8d2a4523e14a966.jpg";
-logoimage[3] = "https://telegra.ph/file/b61382ac648c3957a62af.jpg";
-logoimage[4] = "https://telegra.ph/file/a51a91c22de055d730c6e.jpg";
-
-  var i = Math.floor(5*Math.random())
-
-  var uplogonew = logoimage[i]    
-
-amazone.addCommand({pattern: 'update$', fromMe: true,  desc: Lang.UPDATER_DESC}, (async (message, match) => {
+   var i = Math.floor(2*Math.random())
+   var uploadlogo = image[i]    
+   
+Jimbrootan.addCommand({pattern: 'update$', fromMe: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
+        var webimage = await axios.get(`${uploadlogo}`, { responseType: 'arraybuffer' })
+        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: '\n' + Lang.UPDATE +'\n\n *© Jimbrootan v3*' })
         
-        var webimage = await axios.get(`${uplogonew}`, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: '\n\n\n\n' + Lang.UPDATE +'\n\n\n\n *👩‍🦰powerd by amazone Alexa*' })
-    
     } else {
-        var newzels = Lang.NEW_UPDATE;
+        var jimbru = Lang.NEW_UPDATE;
         commits['all'].map(
             (commit) => {
-                newzels += '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' ◁◁' + commit.author_name + '▷▷\n';
+                jimbru += '🔖 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <' + commit.author_name + '>\n';
             }
         );
         
-        var webimage = await axios.get(`${uplogonew}`, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: newzels + '```'+'\n\n *👩‍🦰powerd by amazone Alexa*' })
-        
+        var webimage = await axios.get(`${uploadlogo}`, { responseType: 'arraybuffer' })
+        await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg  , caption: jimbru + '```'+'\n\n *© Jimbroottan v3 *' })
     }
- }));  
+}));
 
-
-
-amazone.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_DESC}, (async (message, match) => {
+Jimbrootan.addCommand({pattern: 'update now$', fromMe: true , desc: Lang.UPDATE_NOW_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
@@ -100,7 +92,7 @@ amazone.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_
                     exec('npm install').stderr.pipe(process.stderr);
                 } else if (err) {
                     await message.client.sendMessage(
-                        message.jid,'*❌ Have a Error write our support group get more help*\n*Hata:* ```' + err + '```', MessageType.text);
+                        message.jid,'*❌ Güncelleme başarısız oldu!*\n*Hata:* ```' + err + '```', MessageType.text);
                 }
             }));
             await guncelleme.delete();
